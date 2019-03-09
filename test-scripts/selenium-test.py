a54b7a0f176945454e8680175780df27b5f06521
asrsa <aljaz.srsa@gmail.com>
2019-03-09 15:33:00 +0100

    added selenium example

--- test-scripts/selenium-test.py
+++ test-scripts/selenium-test.py
@@ -1,18 +1,30 @@
-import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument("--headless")
-#options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-
-#driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'))
+options.headless = True
 driver = webdriver.Chrome('chromedriver', options=options)
-driver.get("http://fri.uni-lj.si")
 
-for n in driver.find_elements_by_class_name('news-container-title'):
-    if len(n.text) > 0:
-        print(n.text)
+try:
+    driver.get("https://yts.am/")
 
-driver.close()
+    # get all parts of HTML that contain information on titles, genres, years...
+    elems = driver.find_elements_by_xpath('//div[contains(@class, "browse-movie-wrap")]')
+
+    for elem in elems:
+        # some parts doesn't have ratings - check and continue only if there is a rating
+        ratings = elem.find_elements_by_class_name('rating')
+        if len(ratings) == 1:
+            # capture the rating and if it is 7 or more, print the movie title, year and rating
+            # rating comes in the following form: "X / 10" where X is the rating.
+            rating = float(elem.find_element_by_class_name('rating').get_attribute('innerText').split('/')[0])
+            if rating >= 7.0:
+                print(elem.find_element_by_class_name('browse-movie-title').text)
+                print(elem.find_element_by_class_name('browse-movie-year').text)
+                print(rating)
+
+    driver.quit()
+
+except:
+    driver.quit()
+    raise
