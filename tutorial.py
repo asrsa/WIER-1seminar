dd2707c8ad2e6a347237ab40851cae46883c4960
gs8150 <44314817+gs8150@users.noreply.github.com>
2019-03-10 18:12:16 +0100

    Download files/media into user input's folder

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,9 @@
+import os
 from queue import LifoQueue
 
 import psycopg2
+import wget as wget
+
 from config import config
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -53,6 +56,10 @@ driver = webdriver.Chrome(options=chrome_options)
 # frontier init
 frontier.put('http://evem.gov.si')
 
+path = input('Enter folder name to download the media: ')
+if not os.path.exists(path):
+    os.mkdir(path)
+
 while not frontier.empty():
     # deque from LIFO
     urlLink = frontier.get()
@@ -73,9 +80,15 @@ while not frontier.empty():
         # print(link.get('href'))
         url = str(link.get('href'))
         # do not store JPG, PDF, PPT, MP4
+        if '.mp4' in url or '.ppt' in url or '.doc' in url or '.pdf' in url or '.rar' in url or '.zip' in url:
+            try:
+                media = wget.download(url, out=str(path + '/'))
+            except:
+                pass
+            continue
         if 'http' in url and 'gov.si' in url and url not in visited and 'jpg' not in url and 'ppt' not in url \
-                and 'mp4' not in url and 'pdf' not in url and '.zip' not in url and '.rar' not in url and '.xml' not in url\
-                and '.doc' not in url and '@' not in url and 'linkedin' not in url and 'facebook' not in url and 'evem' in url  :
+                and 'mp4' not in url and 'pdf' not in url and '.zip' not in url and '.rar' not in url and '.xml' not in url \
+                and '.doc' not in url and '@' not in url and 'linkedin' not in url and 'facebook' not in url and 'evem' in url:
             frontier.put(link.get('href'))
 
 driver.quit()
